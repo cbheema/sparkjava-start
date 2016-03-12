4441a0fea65d2ae3d83c3512f5d9e05162da494f
cbheema <cbheema@gmail.com>
2016-03-12 15:01:12 +0530

    adding code for spark

---
+++ src/main/java/beamprojects/App.java
@@ -0,0 +1,18 @@
+package beamprojects;
+
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        get("/hello", (req, res) -> "Hello World");
+    	System.out.println( "Hello World!" );
+    }
+}
